f8037eee5b34c50ffe25ce33fc72736772d46d22
xXMarcHerculesXx <mivhercules@gmail.com>
2026-01-15 00:14:16 +0200

    Add files via upload
    Day 1. End of day project that takes two string inputs from user, concatenates the inputs together and then prints it as a "Band name".

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,5 @@
+greeting = "Welcome to the Band Name Generator."
+print(greeting)
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be: " + city + " " + pet_name)
